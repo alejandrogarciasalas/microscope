195dfa46e330b9418709a1a891e1f92e713922d2
alejandro <alejandrogarcia@outlook.com>
2015-02-17 12:18:46 -0500

    Wired collection into postsList template.

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,18 +1,5 @@
-var postsData = [
-	{
-		title: 'Google',
-		url: 'https://www.google.com'
-	},
-	{
-		title: 'Twitter',
-		url: 'https://www.twitter.com'
-	},
-	{
-		title: 'Facebook',
-		url: 'https://www.facebook.com'
-	}
-];
-
 Template.postsList.helpers({
-	posts: postsData
+	posts: function() {
+		return Posts.find();
+	}
 });
